40c28d2e548816bcae2e98f01b2bd81daae4dbb4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-25 14:12:42 +0900

    display usage

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s PATTERN\n", os.Args[0])
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		usage()
+		os.Exit(2)
+	}
+	fmt.Println(args)
+}
