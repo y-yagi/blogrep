b0e2bfab06531acffa27ed1a96c4a1740b5bc279
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-03-26 08:10:42 +0900

    search pattern and display result

--- blogrep.go
+++ blogrep.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage: %s PATTERN\n", os.Args[0])
 }
 
-func readAndGrep(path string, info os.FileInfo, err error) error {
-	fmt.Println(path)
-	return nil
+func readAndGrep(pattern string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		var articles []string
+
+		if info.IsDir() {
+			return nil
+		}
+
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		defer in.Close()
+
+		data, err := ioutil.ReadAll(in)
+		if err != nil {
+			return err
+		}
+		articles = strings.Split(string(data), "***")
+
+		for _, article := range articles {
+			if strings.Contains(article, pattern) {
+				fmt.Fprintln(os.Stdout, article)
+			}
+		}
+
+		return nil
+	}
 }
 
 func main() {
@@ -21,7 +49,5 @@ func main() {
 		usage()
 		os.Exit(2)
 	}
-	fmt.Println(args)
-
-	filepath.Walk(".", readAndGrep)
+	filepath.Walk("testdata", readAndGrep(args[0]))
 }
